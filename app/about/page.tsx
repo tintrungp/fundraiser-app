bbc17eebad34941ac8cb66ac8816cfad29485522
Tin-Trung Pham <tintrungpham1@gmail.com>
2026-05-08 16:55:25 -0700

    Update #1 lol

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,13 +13,49 @@ export default function About() {
             church called Redeemer Presybterian in Encinitas. 
           </p>
           <p className="text-[#6B6B6B]">
-            My time in San Diego has felt like quite the journey. It has place that has gone from
+            My time in San Diego has felt like quite the journey. It has become a place that has gone from
             unfamiliar (when I first arrived), to home (when I met all my college besties), back to 
             unfamiliar (when they all left rip), and slowly into home (meeting a whole lot of great 
             people) again.
           </p>
           <p className="text-[#6B6B6B]">
-            I still need to reflect and write on this, coming soon haha...
+            As I reflect on what has brought me to this moment in time, I can confidently point to Christ having
+            a hand in all of it. Before college, my faith was my identity because I was blessed to be raised 
+            in a Christian household, surrounded by loving parents and siblings who modeled Christ for me. While 
+            seeing that definitely encouraged me, I really felt like I was going through the motions. It wasn't 
+            until going to college that my faith became my own. Through the challenges of seeking out community 
+            God guided me and drew me near. He blessed me with great roommates throughout my time in college, guys 
+            that would become brothers and mentors of my faith. I believe that it was in these formative years that 
+            a desire for the Lord began to stir within me. And so when I graduated and found myself staying in San 
+            Diego, I decided to make the formal declaration of my faith, being baptized at Redeemer.
+          </p>
+          <p className="text-[#6B6B6B]">
+            Indeed, that was the beginning of a new chapter. I do not know what I expected, but it wasn't what ended 
+            up happening. A lot of my community left San Diego or grew a bit distant. As I entered the young adult 
+            stage of life, it felt quite lonely and isolating. I felt like it was difficult to connect, difficult 
+            to put myself out there, and difficult to feel at home despite being with my church family. This was 
+            made more jarring as I felt I was putting in work to be disciplined spiritually, and yet felt so dry.
+            There was a great divide within my heart that I couldn't quite put a finger on. That a fruitful 
+            spiritual life is not built solely upon a disciplined life, but one that is nurtured and walked 
+            alongside community. 
+          </p>
+          <p className="text-[#6B6B6B]">
+            Which brings us to the present day. After a few years of young adult life, the Lord has nudged me 
+            toward fellowship and community within the church. Connections that strengthened my desire for 
+            the Lord, held me accountable in discipline, and supported me in hardship. To this, my cup has 
+            overflowed. With gratitude and thankfulness, I see the work of the Lord at hand. I see the 
+            progress of transformation within my own heart. I see the impact of the love which comes from 
+            knowing Christ. 
+          </p>
+          <p className="text-[#6B6B6B]">
+            And so, here I sit. Writing an about blurb for a fundraising page as I raise funds and support 
+            for both Hope for San Diego and a London Mission Trip with Redeemer. Because in this transformation, 
+            the Lord has blessed me with margin to be able to share the joy, the gratitude, and the love 
+            with those around me. It is in Him and through Him that I write these things. And I hope and pray 
+            that my affections for the Lord would continue to grow, that my love for Him would continue to 
+            overflow toward my neighbors whether they be in San Diego or across the globe, that my life 
+            would be a pleasing sacrifice to the Lord. And I thank you dearly for being a means by which 
+            the Lord has moved my heart to seek such things.
           </p>
         </div>
       </div>
